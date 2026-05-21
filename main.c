d49a130d7bcba31eb633ef1e4399232a9e79bfa1
Luca Gorayeb <lucagorayeb@gmail.com>
2026-05-21 11:49:46 -0400

    Fazendo testes com o banco de dados.

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 int main(){
     sqlite3 *db;
     char *zErrMsg = 0;
-    char *sql;
-    char nome_db = "teste.db";
-
-    create(db, zErrMsg, sql, nome_db);
+    //char *sql = calloc(100, sizeof(char));
+    //char *nome_db = calloc(20, sizeof(char));
+    char *sql = "CREATE TABLE user (""id INTEGER PRIMARY KEY," "nome VARCHAR(10) NOT NULL," "idade INT NOT NULL"");";
+    char *nome_db = "teste.sqlite3";
+     
+    //printf("Digite o nome da database: ");
+    //scanf("%s", nome_db);
     
+    //printf("Digite o sql que deseja executar: ");
+    //scanf("%s", sql);
+    faz_query(db, zErrMsg, sql, nome_db);
 }
